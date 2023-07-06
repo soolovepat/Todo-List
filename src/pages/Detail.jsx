4e3a12e084f68a494c8a08a15fde5cf7e321bdb6
Sujin Lee <soolovepat@naver.com>
2023-07-07 00:21:34 +0900

    Move components into features

--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { useParams, useNavigate } from "react-router-dom";
-import TodoEdit from "../components/TodoEdit";
+import TodoEdit from "../features/TodoEdit";
 import { editTodo } from "../Redux/modules/todos";
 import { StDetail } from "../styles/DetailStyle";
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Header from "../components/Header";
-import TodoCreate from "../components/TodoCreate";
-import TodoList from "../components/TodoList";
+import TodoCreate from "../features/TodoCreate";
+import TodoList from "../features/TodoList";
 
 const Home = () => {
   return (
