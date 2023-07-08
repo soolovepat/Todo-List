8370979d6dd454aaaf98cd8594634b5b07409933
Sujin Lee <soolovepat@naver.com>
2023-07-08 16:23:52 +0900

    Update style

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import Header from "../components/Header";
 import TodoCreate from "../features/TodoCreate";
 import TodoList from "../features/TodoList";
 import Layout from "../components/Layout";
